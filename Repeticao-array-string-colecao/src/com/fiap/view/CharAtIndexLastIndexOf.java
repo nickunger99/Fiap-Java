4de8df4c3189c5fc8364ea0e86a213fadc691aca
Nicolas Unger <nicolas.unger@yahoo.com.br>
2022-01-20 13:15:28 -0300

    Métodos de Strings

---
+++ Repeticao-array-string-colecao/src/com/fiap/view/CharAtIndexLastIndexOf.java
@@ -0,0 +1,18 @@
+package com.fiap.view;
+
+public class CharAtIndexLastIndexOf {
+
+	public static void main(String[] args) {
+		
+		String descricao = new String("Maça Gala, a maça mais doce do mercado!");
+		
+		System.out.println(descricao.charAt(1));
+		
+		System.out.println(descricao.indexOf("G"));
+		System.out.println(descricao.indexOf("Gala"));
+		
+		System.out.println(descricao.indexOf("a"));
+		System.out.println(descricao.lastIndexOf("a"));
+		
+	}
+}
---
+++ Repeticao-array-string-colecao/src/com/fiap/view/LenghtStartsEnds.java
@@ -0,0 +1,17 @@
+package com.fiap.view;
+
+public class LenghtStartsEnds {
+
+	public static void main(String[] args) {
+		
+		String descricao = new String("Maça Gala, a maça mais doce do mercado!");
+		
+		System.out.println(descricao.length());
+		
+		System.out.println(descricao.startsWith("Maça"));
+		System.out.println(descricao.endsWith("!"));
+		
+		
+		
+	}
+}
